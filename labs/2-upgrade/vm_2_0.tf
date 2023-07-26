01d5b6e9a276dd40ba039d6774d8bde45e439bda
tips4you <40390368+tips4you@users.noreply.github.com>
2023-07-26 11:26:30 -0400

    Create vm_2_0.tf

---
+++ labs/2-upgrade/vm_2_0.tf
@@ -0,0 +1,96 @@
+# Refer: https://registry.terraform.io/providers/hashicorp/azurerm/2.0.0/docs/resources/virtual_machine
+# Specify the version of the AzureRM Provider to use
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+# Configure the Azure Provider
+provider "azurerm" {
+  version = "= 2.0.0"  # Use the appropriate version
+
+  subscription_id = "82d00252-32fe-4954-a963-094939b02e1da"
+  client_id       = "da3b0ccb-e3e3-40f4-82a5-3558fe8a0fb8b"
+  client_secret   = "Dlz8Q~vj6hv.xozINfs6q6bboe7VCr2HR5ewocyfc" 
+  tenant_id       = "98244c4f-4e44-4118-a606-e5ded96b09c7d"
+
+  features {}
+}
+
+variable "prefix" {
+  default = "tfvmex"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "Canada Central"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS3_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   =  "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}

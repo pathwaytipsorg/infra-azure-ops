024aea9857f5d17666a50ebc85b3e4c2c20bd3d8
tips4you <40390368+tips4you@users.noreply.github.com>
2023-07-28 16:04:57 -0400

    Create subnet.tf

---
+++ labs/3-splitting-resources/subnet.tf
@@ -0,0 +1,20 @@
+# Create a subnet within the virtual network
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes       = ["10.0.2.0/24"] 
+}
+
+# Network interface card attached to a virtual machine and is used to enable network connectivity for the VM
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}

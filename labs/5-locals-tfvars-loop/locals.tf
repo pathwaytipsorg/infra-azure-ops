11e1d913b60227d0a01b73e381496f0cf71701be
tips4you <40390368+tips4you@users.noreply.github.com>
2023-08-09 15:11:40 -0400

    Update locals.tf

--- labs/5-locals-tfvars-loop/locals.tf
+++ labs/5-locals-tfvars-loop/locals.tf
@@ -6,7 +6,7 @@ and creates project-specific tags using input variables.
 locals {
   owners = var.business_org
   environment = var.environment
-  res_name_prefix = "${var.business_org}-${var.environment}"
+  res_name_prefix = "${local.owners}-${local.environment}"
   proj_tags = {
     owners = local.owners
     environment = local.environment

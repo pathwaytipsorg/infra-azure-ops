29f689748752c395d4a6844b3f55437d6c3b8b72
tips4you <40390368+tips4you@users.noreply.github.com>
2023-08-28 13:30:02 -0400

    Update aks-administrators-azure-ad.tf

--- labs/8-aks-cluster/aks-administrators-azure-ad.tf
+++ labs/8-aks-cluster/aks-administrators-azure-ad.tf
@@ -1,4 +1,4 @@
-# Create Azure AD Group in Active Directory for AKS Admins
+# Create Azure AD Group in Active Directory for AKS Admins. For managing and granting administrative access to an Azure Kubernetes Service (AKS) cluster.
 resource "azuread_group" "aks_administrators" {
   display_name = "${azurerm_resource_group.rg_pathway.name}-cluster-administrators"
   security_enabled = true

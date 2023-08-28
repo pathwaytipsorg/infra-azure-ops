5a8d966d9d824e39051baeb644f9e1b3885fda59
tips4you <40390368+tips4you@users.noreply.github.com>
2023-08-28 13:36:41 -0400

    Update aks-versions-datasource.tf

--- labs/8-aks-cluster/aks-versions-datasource.tf
+++ labs/8-aks-cluster/aks-versions-datasource.tf
@@ -1,4 +1,4 @@
-# Datasource to get Latest Azure AKS latest Version
+# Datasource to get Latest Azure AKS latest Version. "az aks get-versions --location canadacentral -o table"
 data "azurerm_kubernetes_service_versions" "current" {
   location = azurerm_resource_group.rg_pathway.location
   include_preview = false
